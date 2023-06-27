9c45a8c5bdd15fac27a4feb64df50322af6899de
dev4ult <nibrasbiasaaja@gmail.com>
2023-06-27 19:47:45 +0700

    http methods

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
@@ -8,14 +8,29 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/<name>")
-def dashboard(name):
-    return f"Hello {name}"
+@app.route("/auth", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if username == "nibras" and password == "nibras123":
+            return redirect(url_for("main"))
+        else:
+            return render_template(
+                "login.html", error_msg="Username atau password salah"
+            )
+    else:
+        return render_template("login.html")
+
+
+@app.route("/<unknown_url>")
+def not_found404(unknown_url):
+    return render_template("not_found404.html", page=unknown_url)
 
 
 @app.route("/main")
 def main():
-    return redirect(url_for("dashboard", name="asdasd"))
+    return redirect(url_for("home"))
 
 
 if __name__ == "__main__":
